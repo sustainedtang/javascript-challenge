ce0ef288bb87a7c605fe9e5694eab139fdfea6f2
sustainedtang <77056068+sustainedtang@users.noreply.github.com>
2021-06-23 12:57:52 -0700

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -12,5 +12,29 @@ tableData.forEach(function(ufoSighting) {
     //
     var row = tbody.append("tr");
 
-}
-)
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+        console.log(key, value);
+
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+var button = d3.select("#filter-btn");
+
+button.on("click", function(event){
+    d3.event.preventDefault();
+    tbody.html("");
+
+var inputElement = d3.select("#datetime");
+var inputValue = inputElement.property("value");
+
+var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+filteredData.forEach(function(dateData){
+    var row = tbody.append("tr");
+    Object.entries(dateData).forEach(function([key, value]){
+    var cell = tbody.append("td");
+    cell.text(value);
+        });
+    });
+});
